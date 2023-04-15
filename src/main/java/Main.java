5e0b13f5c74704403d39881b25259dc08ab737af
pedrosa <mpedrosa2110@gmail.com>
2023-04-15 10:33:00 -0300

    db

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 public class Main {
-    public static void main(String[] args){
-        try{
+    public static void main(String[] args) {
+        try {
             Dbconector db = new Dbconector();
             db.actualizarDpto();
             db.ConsultarDpto();
-        } catch (Exception e){
+        } catch (Exception e) {
 
         }
 
